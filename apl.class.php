4c8078c8672e6270f12342f53d635890f7a71554
Geovanny De Leon <gldeleon@live.com.mx>
2017-06-22 14:14:38 -0500

    Update apl.class.php

--- apl.class.php
+++ apl.class.php
@@ -1,11 +1,12 @@
 <?php
 require_once('lib/nusoap.php');
 
- $oSoapClient = new nusoap_client('http://serviciosweb.soriana.com/RecibeCfd/wseDocRecibo.asmx?wsdl', true); 
-
+ $oSoapClient = new nusoap_client('http://ruta/del/service.asmx?wsdl', true); 
+//parametros a enviar, deben ser en array
 $param = array('XMLCFD' => $xml);
 
 $oSoapClient->loadWSDL();
+//en call colocamos el nombre del metodo a usar
 $respuesta = $oSoapClient->call("RecibeCFD", $param);
 if ($oSoapClient->fault) { 
         echo 'No se pudo completar la operación '.$oSoapClient->getError();
